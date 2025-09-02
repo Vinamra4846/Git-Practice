fe0fdfd9150512dd70df76dde95fd8c25fb392bc
Vinamra Palandurkar <94834748+Vinamra4846@users.noreply.github.com>
2025-09-02 17:27:30 +0530

    addition

--- calci.cpp
+++ calci.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    
+    int num1,num2;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+    int add = num1 + num2;
+    cout<<"The sum is: "<<add<<endl;
     return 0;
 }
