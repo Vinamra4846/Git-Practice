e178cd9ae08c1fd66d00596599c44b956b0f87eb
Vinamra Palandurkar <94834748+Vinamra4846@users.noreply.github.com>
2025-09-02 17:31:15 +0530

    sub

--- calci.cpp
+++ calci.cpp
@@ -8,5 +8,7 @@ int main(int argc, char const *argv[])
     cin>>num1>>num2;
     int add = num1 + num2;
     cout<<"The sum is: "<<add<<endl;
+    int sub = num1 - num2;
+    cout<<"The difference is: "<<sub<<endl;
     return 0;
 }
